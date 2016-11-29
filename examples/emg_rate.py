227df01c34eaeaf4b671124236f9caedfd5811a7
bdriz <hanaerateau@yahoo.fr>
2016-11-29 17:37:17 +0100

    emg display and sending via asc

--- examples/emg_rate.py
+++ examples/emg_rate.py
@@ -20,58 +20,82 @@
 
 import sys
 import myo as libmyo
+from myo import StreamEmg
 from time import clock
 from collections import deque
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 
+fig, ((ax0, ax1), (ax2, ax3), (ax4, ax5), (ax6, ax7)) = plt.subplots(nrows=4, ncols=2)
+ax0.set_ylim([-50, 50])
+ax1.set_ylim([-50, 50])
+ax2.set_ylim([-50, 50])
+ax3.set_ylim([-50, 50])
+ax4.set_ylim([-50, 50])
+ax5.set_ylim([-50, 50])
+ax6.set_ylim([-50, 50])
+ax7.set_ylim([-50, 50])
 
-class EmgRate(libmyo.DeviceListener):
+x = np.zeros(20)
+x = np.resize(x, (8, 20))
+y = np.arange(0,20)
 
-  __slots__ = 'times last_time n'.split()
+lines=[]
+lines.extend(ax0.plot(y,x[0]))
+lines.extend(ax1.plot(y,x[1]))
+lines.extend(ax2.plot(y,x[2]))
+lines.extend(ax3.plot(y,x[3]))
+lines.extend(ax4.plot(y,x[4]))
+lines.extend(ax5.plot(y,x[5]))
+lines.extend(ax6.plot(y,x[6]))
+lines.extend(ax7.plot(y,x[7]))
 
-  def __init__(self, n):
-    super(EmgRate, self).__init__()
-    self.times = deque()
-    self.last_time = None
-    self.n = int(n)
+#### MYO INIT
+libmyo.init()
+try:
+    hub = libmyo.Hub()
+except MemoryError:
+    print("Myo Hub could not be created. Make sure Myo Connect is running.")
 
-  @property
-  def rate(self):
-    if not self.times:
-      return 0.0
-    else:
-      return 1.0 / (sum(self.times) / float(self.n))
+hub.set_locking_policy(libmyo.LockingPolicy.none)
+feed = libmyo.device_listener.Feed()
+hub.run(1000, feed)
+myos = feed.get_connected_devices()
+feed.wait_for_single_device(10)
+myos[0].set_stream_emg(StreamEmg.enabled)
 
-  def on_pair(self, myo, *args):
-    print("on_pair")
+#### OSC init
+client = udp_client.SimpleUDPClient("172.19.2.89", 5005)
 
-  def on_connect(self, myo, *args):
-    print("on_connect")
+#### ANIMATION Functions
+def animate(i):
+    global x
+    myos = feed.get_connected_devices()
 
-  def on_arm_sync(self, myo, *args):
-    print("on_arm_sync")
-    myo.set_stream_emg(libmyo.StreamEmg.enabled)
+    if myos:
+        if myos[0].emg:
+            for i in range(8):
+                newx = np.append(x[i][1:20], [myos[0].emg[i]])
+                x[i] = newx
+                lines[i].set_ydata(x[i])
+            # Send emg via OSC
+            client.send_message("/myo", myos[0].emg)
 
-  def on_emg_data(self, myo, timestamp, emg):
-    t = clock()
-    if self.last_time is not None:
-      self.times.append(t - self.last_time)
-      if len(self.times) > self.n:
-        self.times.popleft()
-    self.last_time = t
+    return tuple(lines)
 
-def main():
-  libmyo.init()
-  hub = libmyo.Hub()
-  listener = EmgRate(50)
-  try:
-    while True:
-      hub.run_once(100, listener)
-      print("\r\033[KEMG Rate:", listener.rate, end='')
-      sys.stdout.flush()
-  finally:
-    hub.stop(True)
-    hub.shutdown()
+#Init only required for blitting to give a clean slate.
+def init():
+    for i in range(8):
+        lines[i].set_xdata(y)
+    return tuple(lines)
 
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
+    interval=25, blit=True)
 
-if __name__ == '__main__':
-  main()
+plt.show()
+
+hub.stop(True)
+hub.shutdown()
